e04271fa613b9349d733bcc1f8c2dc1e1508e387
Daniel López Plaza <daniel.lp1094@gmail.com>
2021-10-08 14:37:58 +0200

    FEAT: iteration#1

---
+++ iteration-1/iteration-1.js
@@ -0,0 +1,43 @@
+// 1.1------------------------------------------------------------------>
+const $$button = document.body.querySelector('.showme');
+console.log($$button)
+// 1.2------------------------------------------------------------------>
+const $$title = document.body.querySelector('#pillado');
+console.log($$title);
+// 1.3------------------------------------------------------------------>
+const $$paragraph = document.body.querySelectorAll('p');
+console.log($$paragraph );
+// 1.4------------------------------------------------------------------>
+const $$h4 = document.body.querySelectorAll('.pokemon');
+console.log($$h4);
+// 1.5------------------------------------------------------------------>
+const $$span = document.body.querySelectorAll('[data-function = "testMe"]');//Selecciona por atributo de la etiqueta y su valor.
+console.log($$span);
+console.log($$span[2])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
